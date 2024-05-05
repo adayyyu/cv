9d95f33e221117c972252124b246647876f23b4e
ayuyydev <adayyy.hk@gmail.com>
2024-05-05 10:56:56 +0100

    fix: fix type conflicts

--- src/components/JobSection.tsx
+++ src/components/JobSection.tsx
@@ -1,73 +1,144 @@
-import { useState, useCallback } from 'react';
-import { Timeline, Text, Badge, Center, Box, Title, Flex, Container } from '@mantine/core';
-import { useHover } from '@mantine/hooks';
-import ReactMarkdown from 'react-markdown';
+import { useState, useCallback } from "react";
+import {
+  Timeline,
+  Text,
+  Badge,
+  Center,
+  Box,
+  Title,
+  Flex,
+  Container,
+} from "@mantine/core";
+import { useHover } from "@mantine/hooks";
+import ReactMarkdown from "react-markdown";
 import { myJobExp } from "../data";
-import { JobExprience, WorkDuration } from '../types/jobExperience.type';
-import { IconCalendarEvent, IconWorld, IconBriefcase, IconBuilding } from '@tabler/icons-react';
+import { type JobExprience, WorkDuration } from "../types/jobExperience.type";
+import {
+  IconCalendarEvent,
+  IconWorld,
+  IconBriefcase,
+  IconBuilding,
+} from "@tabler/icons-react";
 
-const CompanyName = ({companyName, companyLink} :{companyName: string, companyLink?:string}) => {
-  if(companyLink){
-    return <Text size="sm" ml={5} mr={10} component="a" href={companyLink} target="_blank">{companyName}</Text>;
+const CompanyName = ({
+  companyName,
+  companyLink,
+}: {
+  companyName: string;
+  companyLink?: string;
+}) => {
+  if (companyLink) {
+    return (
+      <Text
+        size="sm"
+        ml={5}
+        mr={10}
+        component="a"
+        href={companyLink}
+        target="_blank"
+      >
+        {companyName}
+      </Text>
+    );
   }
-  return <Text size="sm" ml={5} mr={10}>{companyName}</Text>;
-}
-
-const JobDescriptions = ({descriptions} :{descriptions: string}) => {
-  return <ReactMarkdown children={descriptions} />;
-}
-
-const WorkDurationText = ({workDuration} :{workDuration: WorkDuration}) => {
-  return (<Text size="xs">
-            <Center inline>
-            <IconBriefcase size="1.125rem"/>
-            <Box mx={5}><Text size="md">{workDuration.title}</Text></Box>
-            <IconCalendarEvent size="1.125rem"/>
-            <Box mx={5}>{workDuration.fromDate.month} {workDuration.fromDate.year} to {workDuration.toDate?.month} {workDuration.toDate?.year}</Box>
-            <Badge variant="outline">{workDuration.mode}</Badge>
-          </Center>
-        </Text>);
+  return (
+    <Text size="sm" ml={5} mr={10}>
+      {companyName}
+    </Text>
+  );
 };
 
-const JobExprience = ({data } :{data: JobExprience}) => {
+const JobDescriptions = ({ descriptions }: { descriptions: string }) => {
+  return <ReactMarkdown children={descriptions} />;
+};
+
+const WorkDurationText = ({ workDuration }: { workDuration: WorkDuration }) => {
   return (
-        <Container fluid>
-          <Flex maw='100%' wrap="wrap">
-              <Center inline><IconBuilding size="1.125rem"/><CompanyName companyName={data.companyName} companyLink={data.companyUrl}/></Center>
-              <Center><IconWorld size="1.125rem"/><Text size="sm" ml={5}>{data.location}</Text></Center></Flex>
-          {data.workDuration.map(workDuration => <WorkDurationText workDuration={workDuration} key={`${workDuration.title} ${workDuration.fromDate.year}`}/>)}
-          <Box className="job-description" w='90%'>
-            <JobDescriptions descriptions={data.descriptions}/>
-          </Box>
-          
-        </Container>
-    );
-}
+    <Text size="xs">
+      <Center inline>
+        <IconBriefcase size="1.125rem" />
+        <Box mx={5}>
+          <Text size="md">{workDuration.title}</Text>
+        </Box>
+        <IconCalendarEvent size="1.125rem" />
+        <Box mx={5}>
+          {workDuration.fromDate.month} {workDuration.fromDate.year} to{" "}
+          {workDuration.toDate?.month} {workDuration.toDate?.year}
+        </Box>
+        <Badge variant="outline">{workDuration.mode}</Badge>
+      </Center>
+    </Text>
+  );
+};
+
+const JobExprience = ({ data }: { data: JobExprience }) => {
+  return (
+    <Container fluid>
+      <Flex maw="100%" wrap="wrap">
+        <Center inline>
+          <IconBuilding size="1.125rem" />
+          <CompanyName
+            companyName={data.companyName}
+            companyLink={data.companyUrl}
+          />
+        </Center>
+        <Center>
+          <IconWorld size="1.125rem" />
+          <Text size="sm" ml={5}>
+            {data.location}
+          </Text>
+        </Center>
+      </Flex>
+      {data.workDuration.map((workDuration) => (
+        <WorkDurationText
+          workDuration={workDuration}
+          key={`${workDuration.title} ${workDuration.fromDate.year}`}
+        />
+      ))}
+      <Box className="job-description" w="90%">
+        <JobDescriptions descriptions={data.descriptions} />
+      </Box>
+    </Container>
+  );
+};
 
 const JobSection = () => {
-    const [currentActive, setCurrentActive] = useState(myJobExp.length+1);
-    return (
-      <Box>
-        <Title order={3}>Work Experience</Title>
-        <Timeline mt={5} active={myJobExp.length-currentActive} reverseActive bulletSize={24} lineWidth={2}>
-          {myJobExp.map((item: JobExprience, idx:number)=>{
-            const { hovered, ref } = useHover();
-            const onHover = useCallback(()=>{
-              setCurrentActive(idx+1);
-            },[]);
-            const onMouseOut = useCallback(()=>{
-              setCurrentActive(myJobExp.length+1);
-            },[]);
-            return (
-              <Timeline.Item bullet={hovered} onMouseOver={onHover} onMouseOut={onMouseOut} active={hovered} ref={ref} bg={hovered?'blue.0':undefined} key={`${item.companyName}`}>
-                <JobExprience data={item}></JobExprience>
-              </Timeline.Item>
-            );
-          }
-        )}
-      </Timeline></Box>
-      )
-}
-
+  const [currentActive, setCurrentActive] = useState(myJobExp.length + 1);
+  return (
+    <Box>
+      <Title order={3}>Work Experience</Title>
+      <Timeline
+        mt={5}
+        active={myJobExp.length - currentActive}
+        reverseActive
+        bulletSize={24}
+        lineWidth={2}
+      >
+        {myJobExp.map((item: JobExprience, idx: number) => {
+          const { hovered, ref } = useHover();
+          const onHover = useCallback(() => {
+            setCurrentActive(idx + 1);
+          }, []);
+          const onMouseOut = useCallback(() => {
+            setCurrentActive(myJobExp.length + 1);
+          }, []);
+          return (
+            <Timeline.Item
+              bullet={hovered}
+              onMouseOver={onHover}
+              onMouseOut={onMouseOut}
+              active={hovered}
+              ref={ref}
+              bg={hovered ? "blue.0" : undefined}
+              key={`${item.companyName}`}
+            >
+              <JobExprience data={item}></JobExprience>
+            </Timeline.Item>
+          );
+        })}
+      </Timeline>
+    </Box>
+  );
+};
 
 export default JobSection;
--- src/components/LanguageSection.tsx
+++ src/components/LanguageSection.tsx
@@ -1,32 +1,51 @@
-import { Progress, Text, Title } from '@mantine/core';
-import { languages } from '../data';
-import { Language, LanguageLevel } from '../types/language.type';
-import { usePrintContext } from '../contexts/print';
+import { Progress, Text, Title } from "@mantine/core";
+import { languages } from "../data";
+import { type Language, LanguageLevel } from "../types/language.type";
+import { usePrintContext } from "../contexts/print";
 
-const Language = ({ language, isPrinting }: { language:Language, isPrinting:boolean }) => {
-    const progress = language.level==LanguageLevel.MotherTongue?100:language.level==LanguageLevel.Fluent?75:50;
+const Language = ({
+  language,
+  isPrinting,
+}: {
+  language: Language;
+  isPrinting: boolean;
+}) => {
+  const progress =
+    language.level == LanguageLevel.MotherTongue
+      ? 100
+      : language.level == LanguageLevel.Fluent
+      ? 75
+      : 50;
 
-    return isPrinting?(
-        <Text>{language.name}: {language.level}</Text>
-    ):(
-        <>
-            <Text>{language.name}</Text>
-            <Text size='xs'>{language.level}</Text>
-            <Progress value={progress} size="xs" radius="xs" />
-        </>
-    );
-}
+  return isPrinting ? (
+    <Text>
+      {language.name}: {language.level}
+    </Text>
+  ) : (
+    <>
+      <Text>{language.name}</Text>
+      <Text size="xs">{language.level}</Text>
+      <Progress value={progress} size="xs" radius="xs" />
+    </>
+  );
+};
 
 const LanguageSection = () => {
-    const {isPrinting} = usePrintContext();
-    return (
+  const { isPrinting } = usePrintContext();
+  return (
     <>
-        <Title order={3}>Languages</Title>
-        {languages.map((language)=>{
-            return <Language isPrinting={isPrinting} language={language} key={language.name}/>
-        })}
+      <Title order={3}>Languages</Title>
+      {languages.map((language) => {
+        return (
+          <Language
+            isPrinting={isPrinting}
+            language={language}
+            key={language.name}
+          />
+        );
+      })}
     </>
-    );
-}
+  );
+};
 
 export default LanguageSection;
