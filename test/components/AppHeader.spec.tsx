738aa30901c57dae82cd238587bcb6fa6503aeec
Ada Yu <adayyy.hk@gmail.com>
2024-11-03 18:00:10 +0000

    style: prettier fix

--- test/components/AppHeader.spec.tsx
+++ test/components/AppHeader.spec.tsx
@@ -1,76 +1,73 @@
-import * as React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { vi } from 'vitest';
-import { act } from 'react-dom/test-utils';
-import App from '../../src/App';
+import * as React from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { vi } from "vitest";
+import { act } from "react-dom/test-utils";
+import App from "../../src/App";
 
-describe('<AppHeader/>', () => {
-    it('should show avatar when not printing', async () => {
-        await act(async ()=>{
-            render(<App />);
-        });
-    
-        //avatar is shown by default
-        const img = screen.queryByRole('img');
-        expect(img).toBeTruthy();
-        
+describe("<AppHeader/>", () => {
+  it("should show avatar when not printing", async () => {
+    await act(async () => {
+      render(<App />);
+    });
+
+    //avatar is shown by default
+    const img = screen.queryByRole("img");
+    expect(img).toBeTruthy();
+  });
+
+  it("should show progress bar for languages when not printing", async () => {
+    await act(async () => {
+      render(<App />);
+    });
+
+    //language bars are shown by default
+    const bars = screen.queryAllByRole("progressbar");
+    expect(bars).toBeTruthy();
+  });
+
+  it("should hide avatar when printing", async () => {
+    vi.useFakeTimers();
+    let windowSpy = vi.spyOn(window, "print");
+    await act(async () => {
+      render(<App />);
+    });
+
+    const printButton = screen.getByRole("button", { name: /print/i });
+    expect(printButton).toBeTruthy();
+    if (printButton) {
+      act(() => {
+        fireEvent.click(printButton);
       });
-    
-    it('should show progress bar for languages when not printing', async () => {
-        await act(async ()=>{
-            render(<App />);
-        });
 
-        //language bars are shown by default
-        const bars = screen.queryAllByRole('progressbar')
-        expect(bars).toBeTruthy();
-    
+      // avatar is hidden on print
+      const img = screen.queryByRole("img");
+      expect(img).toBeFalsy();
+
+      act(() => vi.advanceTimersByTime(300));
+      expect(windowSpy).toBeCalledTimes(1);
+    }
+  });
+
+  it("should hide language bars when printing", async () => {
+    vi.useFakeTimers();
+    let windowSpy = vi.spyOn(window, "print");
+    await act(async () => {
+      render(<App />);
     });
 
-    it('should hide avatar when printing', async () => {
-        vi.useFakeTimers()
-        let windowSpy = vi.spyOn(window, "print");
-        await act(async ()=>{
-            render(<App />);
-        });
+    const printButton = screen.getByRole("button", { name: /print/i });
+    expect(printButton).toBeTruthy();
+    if (printButton) {
+      act(() => {
+        fireEvent.click(printButton);
+      });
 
-        const printButton = screen.getByRole('button', { name: /print/i });
-        expect(printButton).toBeTruthy();
-        if(printButton){
-            act(()=>{
-                fireEvent.click(printButton);
-            })
+      //language bars are shown by default
+      const bars = screen.queryAllByRole("progressbar");
+      expect(bars.length).toBe(0);
 
-            // avatar is hidden on print
-            const img = screen.queryByRole('img');
-            expect(img).toBeFalsy();
-            
-            act(()=>vi.advanceTimersByTime(300));
-            expect(windowSpy).toBeCalledTimes(1);
-        }
-    });
-
-    it('should hide language bars when printing', async () => {
-        vi.useFakeTimers()
-        let windowSpy = vi.spyOn(window, "print");
-        await act(async ()=>{
-            render(<App />);
-        });
-
-        const printButton = screen.getByRole('button', { name: /print/i });
-        expect(printButton).toBeTruthy();
-        if(printButton){
-            act(()=>{
-                fireEvent.click(printButton);
-            })
-
-            //language bars are shown by default
-            const bars = screen.queryAllByRole('progressbar')
-            expect(bars.length).toBe(0);
-            
-            act(()=>vi.advanceTimersByTime(300));
-            expect(windowSpy).toBeCalledTimes(1);
-    
-        }
-    });
+      act(() => vi.advanceTimersByTime(300));
+      expect(windowSpy).toBeCalledTimes(1);
+    }
+  });
 });
